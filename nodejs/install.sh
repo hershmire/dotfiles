a17e3688da662532f13e1744c507f35af815fb06
Eddie Leffler <757524+hershmire@users.noreply.github.com>
2022-08-02 15:37:34 -0600

    fix: add asdf packages to global

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -4,16 +4,17 @@ if [ ! -z "${ASDF_DIR}" ]
 then
   asdf plugin add nodejs
   asdf install nodejs lts
+  asdf global nodejs lts
   echo "Note: Additional nodejs versions should be managed separately: 'asdf install nodejs <version>'"
 else
   echo "Skip installing nodejs until 'ASDF_DIR' is available. Run 'reload!' or open a new terminal window after the remaining installs complete and try again."
 fi
 
 # https://www.npmjs.com/package/spoof
-if test ! $(which spoof)
-then
-  if test $(which npm)
-  then
-    sudo npm install spoof -g
-  fi
-fi
+# if test ! $(which spoof)
+# then
+#   if test $(which npm)
+#   then
+#     sudo npm install spoof -g
+#   fi
+# fi
--- yarn/install.sh
+++ yarn/install.sh
@@ -4,6 +4,7 @@ if [ ! -z "${ASDF_DIR}" ]
 then
   asdf plugin add yarn
   asdf install yarn latest
+  asdf global yarn latest
   echo "Note: Additional yarn versions should be managed separately: 'asdf install yarn <version>'"
 else
   echo "Skip installing yarn until 'ASDF_DIR' is available. Run 'reload!' or open a new terminal window after the remaining installs complete and try again."
