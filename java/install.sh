4553ce5a0345f213cedc2f5d16c987dd80dc5eeb
Eddie Leffler <757524+hershmire@users.noreply.github.com>
2022-08-03 10:30:36 -0600

    fix: messaging for install logs

--- java/install.sh
+++ java/install.sh
@@ -3,7 +3,7 @@
 if [ ! -z "${ASDF_DIR}" ]
 then
   asdf plugin add java
-  echo "Note: No default Java version set. Manage this with asdf: 'asdf install java <version>'"
+  echo "Manage java versions with asdf: 'asdf install java <version>'"
 else
   echo "Java versions are managed by 'asdf' and is missing 'ASDF_DIR' env var. No default version are set with this install script."
 fi
--- nodejs/install.sh
+++ nodejs/install.sh
@@ -5,12 +5,3 @@ asdf nodejs update-nodebuild
 asdf install nodejs lts
 asdf global nodejs lts
 echo "Note: Additional nodejs versions should be managed separately: 'asdf install nodejs <version>'"
-
-# https://www.npmjs.com/package/spoof
-# if test ! $(which spoof)
-# then
-#   if test $(which npm)
-#   then
-#     sudo npm install spoof -g
-#   fi
-# fi
