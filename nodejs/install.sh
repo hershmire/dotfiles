53ca76d9a60306c789e2034a2823e31ecce501e3
Eddie <hershmire@users.noreply.github.com>
2025-02-07 13:56:02 -0700

    fix: asdf breaking changes (#19)
    Co-authored-by: Eddie Leffler <757524+hershmire@users.noreply.github.com>

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 asdf plugin add nodejs
-asdf nodejs update-nodebuild
+# asdf nodejs update-nodebuild.bash
 asdf install nodejs lts
-asdf global nodejs lts
+asdf set nodejs lts
 echo "Note: Additional nodejs versions should be managed separately: 'asdf install nodejs <version>'"
 
 # https://www.npmjs.com/package/spoof
--- yarn/install.sh
+++ yarn/install.sh
@@ -2,5 +2,5 @@
 
 asdf plugin add yarn &&
 asdf install yarn latest &&
-asdf global yarn latest
+asdf set yarn latest
 echo "Note: Additional yarn versions should be managed separately: 'asdf install yarn <version>'"
