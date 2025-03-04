62392a201b03a0141d642d49feb8464299258834
Eddie Leffler <757524+hershmire@users.noreply.github.com>
2025-03-04 13:52:14 -0700

    fix: updates from new computer setup

--- java/install.sh
+++ java/install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-if [ ! -z "${ASDF_DIR}" ]
-then
+if ! [[ -n "$(asdf plugin list | grep java)" ]]; then
+  echo "Installing java plugin..."
   asdf plugin add java
   echo "Manage java versions with asdf: 'asdf install java <version>'"
-else
-  echo "Java versions are managed by 'asdf' and is missing 'ASDF_DIR' env var. No default version are set with this install script."
 fi
+
+echo "Testing"
+asdf install java latest:openjdk-24
+asdf set -u java latest:openjdk-24
+echo "Global java set to latest:openjdk-24. Any additional versions should be managed via 'asdf install java <version>'."
